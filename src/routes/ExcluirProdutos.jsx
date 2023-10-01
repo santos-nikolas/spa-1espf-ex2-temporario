a9f50615f02fa24640d83b3c87b9c61a095dd115
santos-nikolas <rm551566@fiap.com.br>
2023-10-01 18:44:17 -0300

    implementando exclusão de produtos da tabela/lista

--- src/routes/ExcluirProdutos.jsx
+++ src/routes/ExcluirProdutos.jsx
@@ -6,6 +6,12 @@ export default function ExcluirProdutos(){
     const {id} = useParams();
     document.title = "Lista de Produtos"
 
+    const handleDelete = (id)=>{
+        fetch(`http://localhost:5173/produtos/${id}`,{method: 'delete'})
+        .then(()=> (window.location = '/produtos'))
+        .catch((error)=> console.log(error))
+    }
+
     const produtoRecuperadoPorId = ListaProduto.filter(item => item.id == parseInt(id))[0]
 
     return(
--- src/routes/Produtos.jsx
+++ src/routes/Produtos.jsx
@@ -1,10 +1,37 @@
-import { Link } from "react-router-dom";
-import { ListaProduto } from "../components/ListaProdutos";
-import style from "./Produtos.module.css";
-import {AiTwotoneEdit as Editar} from "react-icons/ai"
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import style from './Produtos.module.css';
+import { AiTwotoneEdit as Editar } from 'react-icons/ai';
 
 export default function Produtos() {
-  document.title = "Lista de Produtos";
+  const [produtos, setProdutos] = useState([]);
+
+  useEffect(() => {
+    // Tenta obter a lista de produtos do localStorage
+    const listaProdutos = JSON.parse(localStorage.getItem('listaProdutos'));
+
+    // Se existir, usa a lista de produtos do localStorage
+    if (listaProdutos) {
+      setProdutos(listaProdutos);
+    } else {
+      // Senão, usa a lista de produtos inicial
+      const listaInicial = [
+        { id: 1, nome: 'Teclado', desc: 'Teclado Mecânico 102 Teclas.', preco: 220 },
+        { id: 2, nome: 'Mouse', desc: 'Mouse sem fio.', preco: 56.98 },
+        { id: 3, nome: 'Monitor', desc: '32" 75Hz.', preco: 2500 }
+      ];
+      setProdutos(listaInicial);
+    }
+  }, []);
+
+  const handleDelete = (id) => {
+    // Atualiza a lista excluindo o produto correspondente ao ID
+    const novaListaProdutos = produtos.filter((item) => item.id !== id);
+    setProdutos(novaListaProdutos);
+
+    // Atualiza o localStorage com a nova lista de produtos
+    localStorage.setItem('listaProdutos', JSON.stringify(novaListaProdutos));
+  };
 
   return (
     <div>
@@ -23,22 +50,27 @@ export default function Produtos() {
         </thead>
 
         <tbody>
-          {ListaProduto.map((item, indice) => (
+          {produtos.map((item, indice) => (
             <tr key={indice} className={style.lineTbl}>
               <td>{item.id}</td>
               <td>{item.nome}</td>
               <td>{item.desc}</td>
               <td>{item.preco}</td>
-              <td><Link to={`/editar/produtos/${item.id}`}> <Editar/> </Link> </td>
-              <td><Link to={`/excluir/produtos/${item.id}`}>X</Link> </td>
+              <td>
+                <Link to={`/editar/produtos/${item.id}`}>
+                  <Editar />
+                </Link>
+              </td>
+              <td>
+                <button onClick={() => handleDelete(item.id)}>Excluir</button>
+              </td>
             </tr>
           ))}
         </tbody>
+
         <tfoot>
           <tr>
-            <td colSpan={6}>
-              PRODUTOS INFORMÁTICOS - QTD = {ListaProduto.length}
-            </td>
+            <td colSpan={6}>PRODUTOS INFORMÁTICOS - QTD = {produtos.length}</td>
           </tr>
         </tfoot>
       </table>
